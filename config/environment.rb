69aa63c6a7b01690770117e96685f8b0d113bb27
Ian Schoonover <iantskon@gmail.com>
2016-12-12 20:16:41 -0600

    Add bootstrap blog theme

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Add created on datetime format
+Time::DATE_FORMATS[:created_on] = "%B %d at %I:%M %p"
