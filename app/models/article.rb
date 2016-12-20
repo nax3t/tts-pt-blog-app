93f282d6ef594b815dd81ffbba3cbda24407bc00
Ian Schoonover <iantskon@gmail.com>
2016-12-19 21:07:23 -0600

    Add flexbox css fix for grid system

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,7 +4,7 @@ class Article < ApplicationRecord
 	has_many :comments, dependent: :destroy
 	belongs_to :user
 	# validations
-	validates :title, presence: true, length: { minimum: 5 }
+	validates :title, presence: true, length: { minimum: 5, maximum: 30 }
 	validates :text, presence: true, length: { minimum: 5 }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 	# geocoder
